142a973010e5be41d13c41e25093574a4a8572b0
Oskar Grunning <oskargrunning@gmail.com>
2019-04-05 15:59:26 +0200

    fix(build): improve buildsystem further

--- rollup.config.js
+++ rollup.config.js
@@ -6,81 +6,78 @@ import { terser } from 'rollup-plugin-terser'
 
 import pkg from './package.json'
 
-const env = process.env.NODE_ENV
+const bundleTarget = process.env.BUILD_BUNDLE
+const dependencies = [
+  ...Object.keys (pkg.dependencies || {}),
+  ...Object.keys (pkg.peerDependencies || {}),
+  'core-js',
+]
+const isNil = (...xs) => xs.some (x => x == null)
+const isObjectLike = x => x != null && typeof x === 'object'
+const isArrayLike = x => x != null && typeof x[Symbol.iterator] === 'function'
+const isSameType = (x, y) => !isNil (x, y) && x.constructor === y.constructor
+const external = id => dependencies.map (dep => id.startsWith (dep)).some (Boolean)
+const sanitizeBundle = ({ type, ...rest }) => rest
 
 const defaults = {
   input: 'src/index.js',
-  output: { exports: 'named', format: env, indent: false },
+  output: { exports: 'named', indent: false },
   plugins: [nodeResolve ({ jsnext: true }), commonjs (), json ()],
   treeshake: true,
 }
-const dependencies = [...Object.keys (pkg.dependencies || {}), 'core-js']
-const external = id => dependencies.map (dep => id.startsWith (dep)).some (Boolean)
+
+const bundle = opts =>
+  Object.entries ({ ...defaults, ...opts }).reduce (
+    (acc, [k, v]) => ({
+      ...acc,
+      [k]:
+        isObjectLike (v) && isSameType (v, defaults[k])
+          ? isArrayLike (v)
+            ? [...defaults[k], ...v]
+            : { ...defaults[k], ...v }
+          : v,
+    }),
+    {}
+  )
+
+const bundles = [
+  bundle ({
+    external,
+    output: { format: 'cjs', file: `lib/${pkg.name}.js` },
+    plugins: [babel ({ runtimeHelpers: true })],
+    type: 'cjs',
+  }),
+  bundle ({
+    external,
+    output: { format: 'es', file: `es/${pkg.name}.mjs` },
+    plugins: [babel ({ runtimeHelpers: true })],
+    type: 'es',
+  }),
+  bundle ({
+    output: { format: 'umd', file: `dist/${pkg.name}.js`, name: pkg.name },
+    plugins: [babel ({ exclude: 'node_modules/**' })],
+    type: 'umd',
+  }),
+  bundle ({
+    output: { format: 'umd', file: `dist/${pkg.name}.min.js`, name: pkg.name },
+    plugins: [
+      babel ({ exclude: 'node_modules/**' }),
+      terser ({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          warnings: false,
+        },
+      }),
+    ],
+    type: 'umd',
+  }),
+]
 
 export default (() =>
-  env === 'cjs'
-    ? [
-        {
-          ...defaults,
-          external,
-          output: { ...defaults.output, file: `lib/${pkg.name}.js` },
-          plugins: [...defaults.plugins, babel ({ runtimeHelpers: true })],
-        },
-      ]
-    : env === 'es'
-    ? [
-        {
-          ...defaults,
-          external,
-          output: { ...defaults.output, file: `es/${pkg.name}.js` },
-          plugins: [...defaults.plugins, babel ({ runtimeHelpers: true })],
-        },
-        {
-          ...defaults,
-          output: { ...defaults.output, file: `es/${pkg.name}.mjs` },
-          plugins: [
-            ...defaults.plugins,
-            terser ({
-              compress: {
-                pure_getters: true,
-                unsafe: true,
-                unsafe_comps: true,
-                warnings: false,
-              },
-            }),
-          ],
-        },
-      ]
-    : env === 'umd'
-    ? [
-        {
-          ...defaults,
-          output: {
-            ...defaults.output,
-            file: `dist/${pkg.name}.js`,
-            name: pkg.name,
-          },
-          plugins: [...defaults.plugins, babel ({ exclude: 'node_modules/**' })],
-        },
-        {
-          ...defaults,
-          output: {
-            ...defaults.output,
-            file: `dist/${pkg.name}.min.js`,
-            name: pkg.name,
-          },
-          plugins: [
-            ...defaults.plugins,
-            babel ({ exclude: 'node_modules/**' }),
-            terser ({
-              compress: {
-                pure_getters: true,
-                unsafe: true,
-                unsafe_comps: true,
-                warnings: false,
-              },
-            }),
-          ],
-        },
-      ]
-    : []) ()
+  [
+    ...(bundleTarget != null
+      ? bundles.filter (x => x.type === bundleTarget)
+      : bundles),
+  ].map (sanitizeBundle)) ()
