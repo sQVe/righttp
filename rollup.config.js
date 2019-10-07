135bab69436efb649b915b1e6ae8ca263a147cd0
Oskar Grunning <oskargrunning@gmail.com>
2019-10-07 11:31:10 +0200

    chore(eslint): update to latest eslint config

--- rollup.config.js
+++ rollup.config.js
@@ -2,37 +2,30 @@ import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import json from 'rollup-plugin-json'
 import nodeResolve from 'rollup-plugin-node-resolve'
-import { terser } from 'rollup-plugin-terser'
 
 import pkg from './package.json'
 
 const bundleTarget = process.env.BUILD_BUNDLE
 const dependencies = [
-  ...Object.keys (pkg.dependencies || {}),
-  ...Object.keys (pkg.peerDependencies || {}),
   'core-js',
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
 ]
-const isNil = (...xs) => xs.some (x => x == null)
-const isObjectLike = x => x != null && typeof x === 'object'
+
 const isArrayLike = x => x != null && typeof x[Symbol.iterator] === 'function'
-const isSameType = (x, y) => !isNil (x, y) && x.constructor === y.constructor
-const external = id => dependencies.map (dep => id.startsWith (dep)).some (Boolean)
+const isNil = (...xs) => xs.some(x => x == null)
+const isObjectLike = x => x != null && typeof x === 'object'
+const isSameType = (x, y) => !isNil(x, y) && x.constructor === y.constructor
 const sanitizeBundle = ({ type, ...rest }) => rest
+const removeRelativePath = dep => dep.replace(/^(\.{1,2}\/)+/, '')
 
-const defaults = {
-  input: 'src/index.js',
-  output: { exports: 'named', indent: false },
-  plugins: [nodeResolve ({ jsnext: true }), commonjs (), json ()],
-  treeshake: true,
-}
-
-const bundle = opts =>
-  Object.entries ({ ...defaults, ...opts }).reduce (
+const presetBundleDefaults = defaults => opts =>
+  Object.entries({ ...defaults, ...opts }).reduce(
     (acc, [k, v]) => ({
       ...acc,
       [k]:
-        isObjectLike (v) && isSameType (v, defaults[k])
-          ? isArrayLike (v)
+        isObjectLike(v) && isSameType(v, defaults[k])
+          ? isArrayLike(v)
             ? [...defaults[k], ...v]
             : { ...defaults[k], ...v }
           : v,
@@ -40,44 +33,51 @@ const bundle = opts =>
     {}
   )
 
+const external = dependencies => id =>
+  dependencies.map(dep => removeRelativePath(id).startsWith(dep)).some(Boolean)
+
+const bundle = presetBundleDefaults({
+  external: external(dependencies),
+  input: 'src/index.ts',
+  output: { exports: 'named', indent: false },
+  plugins: [nodeResolve(), commonjs(), json()],
+  treeshake: true,
+})
+
 const bundles = [
-  bundle ({
-    external,
+  bundle({
     output: { format: 'cjs', file: `lib/${pkg.name}.js` },
-    plugins: [babel ({ runtimeHelpers: true })],
+    plugins: [babel({ runtimeHelpers: true })],
     type: 'cjs',
   }),
-  bundle ({
-    external,
-    output: { format: 'es', file: `es/${pkg.name}.mjs` },
-    plugins: [babel ({ runtimeHelpers: true })],
+  bundle({
+    input: 'src/cli.js',
+    output: { format: 'cjs', file: `lib/cli.js` },
+    plugins: [babel({ runtimeHelpers: true })],
+    type: 'cli',
+  }),
+  bundle({
+    output: { format: 'es', file: `es/${pkg.name}.js` },
+    plugins: [babel({ runtimeHelpers: true })],
     type: 'es',
   }),
-  bundle ({
-    output: { format: 'umd', file: `dist/${pkg.name}.js`, name: pkg.name },
-    plugins: [babel ({ exclude: 'node_modules/**' })],
-    type: 'umd',
+  bundle({
+    output: { format: 'es', file: `es/${pkg.name}.mjs` },
+    plugins: [babel({ runtimeHelpers: true })],
+    type: 'es',
   }),
-  bundle ({
-    output: { format: 'umd', file: `dist/${pkg.name}.min.js`, name: pkg.name },
-    plugins: [
-      babel ({ exclude: 'node_modules/**' }),
-      terser ({
-        compress: {
-          pure_getters: true,
-          unsafe: true,
-          unsafe_comps: true,
-          warnings: false,
-        },
-      }),
-    ],
-    type: 'umd',
-  }),
+  {
+    external: external(dependencies),
+    input: 'src/color-scaper.js',
+    output: { format: 'cjs', file: '.tmp/color-scraper.js' },
+    plugins: [json()],
+    type: 'colors',
+  },
 ]
 
 export default (() =>
   [
     ...(bundleTarget != null
-      ? bundles.filter (x => x.type === bundleTarget)
+      ? bundles.filter(x => x.type === bundleTarget)
       : bundles),
-  ].map (sanitizeBundle)) ()
+  ].map(sanitizeBundle))()
