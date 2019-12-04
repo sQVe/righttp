b8fbf33315362fca5f03545d136500278faa2d98
Oskar Grunning <oskargrunning@gmail.com>
2019-12-04 16:04:01 +0100

    refactor(helpers): only keep base functions in righttp module

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,11 @@
-import { Container, QueryParams } from './types'
+import {
+  Container,
+  Init,
+  Options,
+  QueryParams,
+  ResolveMethod,
+  ResponseResolve,
+} from './types'
 import { compose, isString } from './utility'
 
 /** createQuery :: {k:v} -> String */
@@ -10,18 +17,43 @@ export const createQuery = (params: QueryParams = {}) => {
   return arr.length ? arr.join('&') : ''
 }
 
+/** getResolveMethodName :: String -> String */
+export const getResolveMethodName = (resolveAs: ResponseResolve) => {
+  const caseSafeResolveAs = resolveAs.toLowerCase()
+  const mixedCaseMethodNameLookup: { [index: string]: ResolveMethod } = {
+    arraybuffer: 'arrayBuffer',
+    formdata: 'formData',
+  }
+
+  return mixedCaseMethodNameLookup[caseSafeResolveAs] || caseSafeResolveAs
+}
+
+/** resolveMethod :: (Response, String) -> Promise a */
+export const resolveResponse = async (
+  res: Response,
+  resolveAs: ResponseResolve
+) => {
+  const resolveMethodName = getResolveMethodName(resolveAs)
+
+  if (resolveMethodName === 'json') {
+    const text = await res.clone().text()
+
+    if (text.length === 0) return {}
+    return res.json()
+  } else if (resolveMethodName === 'response') {
+    return res
+  }
+
+  return res[resolveMethodName]()
+}
+
 /** sanitizeUrl :: String -> String */
 export function sanitizeUrl(url?: string): string {
   const addTrailingSlash = (x: string) => (x.endsWith('/') ? x : `${x}/`)
   const removeLeadingSlash = (x: string) =>
     x.startsWith('/') ? x.substring(1) : x
 
-  return isString(url)
-    ? compose(
-        addTrailingSlash,
-        removeLeadingSlash
-      )(url)
-    : ''
+  return isString(url) ? compose(addTrailingSlash, removeLeadingSlash)(url) : ''
 }
 
 /** combineContainers :: {a} -> {a} -> {a} */
@@ -32,3 +64,9 @@ export const combineContainers = (a: Container) => (
   init: { ...a.init, ...b.init },
   options: { ...a.options, ...b.options },
 })
+
+/** preset :: ({a} -> Promise b) -> {a} -> Promise b */
+export const preset = (
+  fn: (container: Container) => Promise<any> // eslint-disable-line @typescript-eslint/no-explicit-any
+) => (container: Container) => (url: string, init: Init, options: Options) =>
+  fn(combineContainers(container)({ url, init, options }))
--- src/righttp.ts
+++ src/righttp.ts
@@ -1,43 +1,6 @@
-import {
-  Container,
-  Init,
-  Options,
-  ResolveMethod,
-  ResponseResolve,
-} from './types'
-import { combineContainers } from './helpers'
+import { Container, Init, Options } from './types'
+import { combineContainers, resolveResponse, preset } from './helpers'
 import { defaultContainer } from './constants'
-
-/** preset :: ({a} -> Promise b) -> {a} -> Promise b */
-const preset = (
-  fn: (container: Container) => Promise<any> // eslint-disable-line @typescript-eslint/no-explicit-any
-) => (container: Container) => (url: string, init: Init, options: Options) =>
-  fn(combineContainers(container)({ url, init, options }))
-
-const getResolveMethodName = (resolveAs: ResponseResolve) => {
-  const caseSafeResolveAs = resolveAs.toLowerCase()
-  const mixedCaseMethodNameLookup: { [index: string]: ResolveMethod } = {
-    arraybuffer: 'arrayBuffer',
-    formdata: 'formData',
-  }
-
-  return mixedCaseMethodNameLookup[caseSafeResolveAs] || caseSafeResolveAs
-}
-
-const resolveResponse = async (res: Response, resolveAs: ResponseResolve) => {
-  const resolveMethodName = getResolveMethodName(resolveAs)
-
-  if (resolveMethodName === 'json') {
-    const text = await res.clone().text()
-
-    if (text.length === 0) return {}
-    return res.json()
-  } else if (resolveMethodName === 'response') {
-    return res
-  }
-
-  return res[resolveMethodName]()
-}
 
 /** request :: {a} -> Promise b */
 const request = async (container: Container) => {
