f00ed8cd99664ba6f6fe512cf8156661fd40ca13
Oskar Grunning <oskargrunning@gmail.com>
2020-01-24 13:47:53 +0100

    test(constants): add tests for our defaults

---
+++ test/constants.test.js
@@ -0,0 +1,22 @@
+import { defaultContainer } from '../src/constants'
+
+describe('constants', () => {
+  beforeEach(() => {
+    global.JSON = {
+      stringify: jest.fn(),
+    }
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should have the correct default settings', () => {
+    const payload = 'JSON'
+    defaultContainer.options.payloadAs(payload)
+
+    expect(defaultContainer).toMatchSnapshot()
+    expect(JSON.stringify).toHaveBeenCalledTimes(1)
+    expect(JSON.stringify).toHaveBeenCalledWith(payload)
+  })
+})
