cf1bff19b532430d3f2f714a13007c5ef8e3206d
Oskar Grunning <oskargrunning@gmail.com>
2020-02-06 22:32:07 +0100

    refactor(utility): remove unused compose utility

--- src/utility.ts
+++ src/utility.ts
@@ -1,11 +1,3 @@
-/** compose :: (b -> c) -> (a -> b) -> a -> c */
-export const compose = (
-  ...fns: any[] // eslint-disable-line @typescript-eslint/no-explicit-any
-) =>
-  fns.reduce((f, g) => (
-    ...args: any[] // eslint-disable-line @typescript-eslint/no-explicit-any
-  ) => f(g(...args)))
-
 /** isNonEmptyString :: String -> Boolean */
 export const isNonEmptyString = (val: unknown): val is string =>
   typeof val === 'string' && val.length > 0
--- test/utility.test.js
+++ test/utility.test.js
@@ -1,19 +1,7 @@
-import { compose, isNonEmptyString } from '../src/utility'
+import { isNonEmptyString } from '../src/utility'
 import { valuesWithDifferentTypes } from './setup/constants'
 
 describe('Utility', () => {
-  describe('compose', () => {
-    it('should compose unary functions right to left', () => {
-      expect(
-        compose(
-          x => x + 'qux',
-          x => x + 'baz',
-          x => x + 'bar'
-        )('foo')
-      ).toBe('foobarbazqux')
-    })
-  })
-
   describe('isNonEmptyString', () => {
     it('should return true for strings', () => {
       expect(isNonEmptyString('foo')).toBe(true)
